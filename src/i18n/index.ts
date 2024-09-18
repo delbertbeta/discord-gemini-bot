306b70ba1e2d14aeed7ded365cb652dc87f97bc1
RuMaxwell <25785855+RuMaxwell@users.noreply.github.com>
2024-09-18 22:46:20 +0800

    fix: i18n format error.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,3 +1,5 @@
+import { formatString } from "../utils";
+
 export class I18n {
   lang: "zh" | string;
   config: Record<string, string | object>;
@@ -21,25 +23,19 @@ export class I18n {
   ): string | undefined {
     const keys = path.split(".");
 
-    let v: any = this.config;
+    let template: any = this.config;
     for (const key of keys) {
-      v = v[key];
-      if (v === undefined) {
+      template = template[key];
+      if (template === undefined) {
         return;
       }
     }
 
-    if (typeof v === "string" && formatArgs) {
-      const matches = v.matchAll(/(?<!\\)\{([^\}]+)\}/g);
-      for (const m of matches) {
-        v =
-          v.slice(0, m.index) +
-          formatArgs[m[1] as any] +
-          v.slice(m.index + m.length);
-      }
+    if (typeof template === "string" && formatArgs) {
+      template = formatString(template, formatArgs);
     }
 
-    return v;
+    return template;
   }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,40 @@ export function replaceWithObjectValues(str: string, obj: Map<string, User>) {
     return `@${obj.get(p1)?.displayName ?? p1}`; // 如果对象中没有对应值，则返回原来的数字
   });
 }
+
+/**
+ * Interpolates a string, replacing curly brace embraced strings with some
+ * arguments.
+ *
+ * @example
+ * formatString("no interpolation", [0]) === "no interpolation"
+ * formatString("{0} + {0} = 2 * {0}", [42]) === "42 + 42 = 2 * 42"
+ * formatString("?id={id}&q={query}", { id: 1, query: "answer" }) === "?id=1&q=answer"
+ */
+export function formatString(
+  template: string,
+  formatArgs: any[] | Record<PropertyKey, any>
+): string {
+  // Finds all "{}" pairs with some text between.
+  const matches = template.matchAll(/(\\?)\{(.+?)\}/g);
+  let result = "";
+  let lastEnd = 0;
+
+  for (const m of matches) {
+    // If the "{}" pair has a preceding "\\", it is treated like a raw pair
+    // of "{}".
+    // e.g. `t("\\{some text}")` === `"{some text}"`
+    if (m[1]) {
+      result += m[0].slice(1);
+    }
+    // If the "{}" pair does not have a preceding "\\", it is replaced by
+    // the corresponding value in `formatArgs`.
+    // e.g. `t("{some text}")` === `formatArgs["some text"]`
+    else {
+      result += template.slice(lastEnd, m.index) + formatArgs[m[2] as any];
+    }
+    lastEnd = m.index + m.length + 1;
+  }
+
+  return result;
+}
